5c7ceb2418c28308af5d8434c52165e64c2392ea
boui <rocksyseletska2837@gmail.com>
2013-08-28 13:31:36 +0300

    hdfs tested

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-PROJECT_NAME="hdfs_test"
+
 VERSION="0.1-SNAPSHOT"
-BASE_DIR="/home/boui/IdeaProjects/${PROJECT_NAME}"
+BASE_DIR="."
 
 TARGET_DIR="$BASE_DIR/target/scala-2.10/"
-JAR_NAME="${PROJECT_NAME}-assembly-${VERSION}"
+
+JAR_NAME="hdfs_test-assembly-${VERSION}"
 
 APP_JAR_PATH="$TARGET_DIR/${JAR_NAME}.jar"
 
 JOB_NAME="$1"
-exec ~/Software/hadoop-1.2.1/bin/hadoop jar "$APP_JAR_PATH" "$JOB_NAME" --local --input ./a.txt --output ./b.txt 
+shift
+
+
+exec hadoop jar "$APP_JAR_PATH" "$JOB_NAME" --hdfs "$@"
  
